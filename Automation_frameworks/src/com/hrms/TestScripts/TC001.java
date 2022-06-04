c854add31ffdfdda74d106afaed2675478ce5992
jagad <paladijagadeesh07@gmail.com>
2022-06-04 20:39:19 +0530

    Adding all FrameWorks - Jagadeesh

---
+++ Automation_frameworks/src/com/hrms/TestScripts/TC001.java
@@ -0,0 +1,10 @@
+package com.hrms.TestScripts;
+
+public class TC001 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Automation_frameworks/src/com/hrms/lib/Global.java
@@ -0,0 +1,17 @@
+package com.hrms.lib;
+import org.openqa.selenium.WebDriver;
+public class Global {
+//----------------------Variables------------------------------
+	public WebDriver driver;
+	public String url ="https://opensource-demo.orangehrmlive.com/";
+	public String un = "Admin";
+	public String pw = "admin123";
+//----------------------Objectives-----------------------------
+	public String txt_loginname = "txtUsername";
+	public String txt_passwordname = "txtPassword";
+	public String txt_loginbutton = "Submit";
+	public String txt_logoutbutton = "Logout";
+	
+
+
+}
